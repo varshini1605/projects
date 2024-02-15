e0809eebad1ec3c16fd98f6ec0af19b431fc8e22
varshini p <60183760+varshini1605@users.noreply.github.com>
2024-02-15 08:38:58 +0000

    Random

--- Content.py
+++ Content.py
@@ -1,1 +1,16 @@
-1
+import csv
+import random
+
+def get_random_Quote(file='list.csv'):
+    try:#Load quotes from csv file
+        with open(file) as csvfile:
+            Quotes=[{'author': line[1],'quotes': line[0]} for line in csv.reader(csvfile, delimiter='|')]
+
+    except Exception as e:#default get_random_Quote
+        Quotes=[{"author":"abc", "quote":'anything'}]
+    return random.choice(Quotes)
+
+if __name__=='__main__':
+    Quotes=get_random_Quote()
+    print(f'quote is "{Quotes["quotes"]}" - {Quotes["author"]}')
+
